9e345487f217d9561572f40c0c85fb5f4d22e357
Robert Rees <spam@shudderfix.com>
2010-07-05 22:51:24 +0100

    Implemented dice rolling

--- src/town_maker/core.clj
+++ src/town_maker/core.clj
@@ -2,3 +2,7 @@
 
 (defn score-maker [a b c d]
 	(reduce + (rest (sort < (vector a b c d)))))
+
+(defn d6[] (+ 1 (rand-int 6)))
+
+(defn dice-rolls [] (lazy-seq (cons (d6) (dice-rolls))))
--- test/town_maker/core_test.clj
+++ test/town_maker/core_test.clj
@@ -7,3 +7,9 @@
 		(= 14 (score-maker 3 4 5 5)))
 	(it "drops the lowest score even for values that are all equal"
 		(= 9 (score-maker 3 3 3 3))))
+
+(describe dice-rolls "rolling dice"
+	(it "is never less than one"
+		(= 0 (count (filter #(< % 1) (take 10000 (dice-rolls))))))
+	(it "is never more than 6"
+		(= 0 (count (filter #(> % 7) (take 10000 (dice-rolls)))))))
